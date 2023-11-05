ce9351640148d5913c5107df9c7de5be367d469e
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2023-11-05 15:15:05 +0700

    Update md2html.py
    Signed-off-by: Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>

--- md2html.py
+++ md2html.py
@@ -79,6 +79,7 @@ footer_styles = """
 
 def generate_h1_tag(filename):
     title = os.path.splitext(filename)[0].capitalize()
+    utc_datetime = datetime.datetime.utcnow()
     h1_tag = f'<h1 align="center">Bảng xếp hạng {title}</h1><p> Lần cuối cập nhật: {utc_datetime.hour}:{utc_datetime.minute}:{utc_datetime.second} UTC, ngày {utc_datetime.day} tháng {utc_datetime.month} năm {utc_datetime.year}</p>'
     return h1_tag
 
